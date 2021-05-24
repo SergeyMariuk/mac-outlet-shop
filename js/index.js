de939d206cdf9d37d431c57837573ae359f126d2
Sergey <sergey@mai.com>
2021-05-25 01:54:14 +0300

    render filter

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
+// Creates random number, required for Orders amount.
 function getRandomInt() {
     min = Math.ceil(100);
     max = Math.floor(1000);
     return Math.floor(Math.random() * (max - min) + min);
   }
 
-
-items.forEach(element => {
-    const caredWrapper = document.createElement('div');
-    caredWrapper.className = 'card-wrapper';
-    caredWrapper.innerHTML = 
+// Renders single card.
+const renderCard = function(element){
+    const cardWrapper = document.createElement('div');
+    cardWrapper.className = 'card-wrapper';
+    cardWrapper.innerHTML = 
     `<div class="card">
         <div class="flex-wrapper">
             <div class="like like-empty"></div>
@@ -43,9 +44,18 @@ items.forEach(element => {
         </div>
         <div class="orders"><span>${getRandomInt()}</span><br>orders</div>
     </div>`
-    document.querySelector('.item-list').appendChild(caredWrapper) 
-});
+    return cardWrapper;
+}
 
+// Fills store by rendered cards.
+const fillStore = function(elements){
+    elements.forEach(element => { 
+        document.querySelector('.item-list').appendChild(renderCard(element)) 
+    });    
+}
+fillStore(items);
+
+// Sets like/unlike
 const like = document.querySelectorAll('.like');
 like.forEach(element => {
     element.onclick = function(){
@@ -53,3 +63,172 @@ like.forEach(element => {
         else element.className = 'like like-empty like-filled'
         }
 });
+
+// Accordion menu.
+const acc = document.getElementsByClassName("section-wrapper");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    this.firstElementChild.classList.toggle("active");
+
+    var arrow = this.lastElementChild;
+    this.lastElementChild.classList.toggle('opened')
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// Verifies if element visible on a page.
+const isVisible = function(element){
+    if(!(element.offsetParent === null)) return true;
+    return false
+}
+
+// Open filter after click on '.filter-icon' and rebuild '.item-list'.
+const filterIcon = document.querySelector('.filter-icon');
+const accordionFilter = document.querySelector('.accordion-filter');
+const itemList = document.querySelector('.item-list');
+const cardWrapper = document.querySelectorAll('.card-wrapper');
+filterIcon.onclick = function(){
+    if(isVisible(accordionFilter)){
+        accordionFilter.style.display = "none";
+        itemList.style.width = "100%";
+        cardWrapper.forEach(element => element.style.width = "30%");
+        cardWrapper.style.width = "30%";
+    }else{
+        accordionFilter.style.display = "block";
+        itemList.style.width = "64%";
+        cardWrapper.forEach(element => element.style.width = "45%");
+    }
+}
+
+// Gets all available devices color.
+const getColor = function(elements){
+    let result = [];
+    const tmpCollorsArr = elements.map(elem => elem.color);
+
+    tmpCollorsArr.forEach(colors => {
+        colors.forEach(color => {
+            if(!result.includes(color)) result.push(color);
+        })
+    })
+
+    return result;
+}
+
+// Renders color checkbox in the filter
+const renderColorCheckbox = function(color){
+    const colorCheckboxWrapper = document.createElement('div');
+    colorCheckboxWrapper.className = 'color-wrapper';
+    colorCheckboxWrapper.innerHTML = `
+    <input type="checkbox">
+    <div class="color-name">${color}</div>`
+    return colorCheckboxWrapper;
+}
+
+// Fills filter by rendered color checkboxes.
+const fillColorFlter = function(elements){
+    elements.forEach(element => { 
+        document.querySelector('.color-panel-wrapper').appendChild(renderColorCheckbox(element))
+    });
+}
+
+fillColorFlter(getColor(items));
+
+const memoryFilterArr = [
+    '32 Gb',
+    '64 Gb',
+    '128 Gb',
+    '256 Gb',
+    '512 Gb',
+    '1 TB',
+    '2 TB',
+    '4 TB'
+];
+
+// Renders memory checkbox in the filter
+const renderMemoryCheckbox = function(memory){
+    const memoryCheckboxWrapper = document.createElement('div');
+    memoryCheckboxWrapper.className = 'memory-wrapper';
+    memoryCheckboxWrapper.innerHTML = `
+    <input type="checkbox">
+    <div class="memory-name">${memory}</div>`
+    return memoryCheckboxWrapper;
+}
+
+// Fills filter by rendered memory checkboxes.
+const fillMemoryFlter = function(elements){
+    elements.forEach(element => { 
+        document.querySelector('.memory-panel-wrapper').appendChild(renderMemoryCheckbox(element))
+    });
+}
+
+fillMemoryFlter(memoryFilterArr);
+
+// Gets all available devices os.
+const getOs = function(elements){
+    const tmpOsArr = elements.map(elem => elem.os);
+    const result = [];
+    tmpOsArr.forEach(os => {
+        if(!result.includes(os)) result.push(os);
+    })
+
+    return result;
+}
+
+// Renders OS checkbox in the filter
+const renderOsCheckbox = function(os){
+    const osCheckboxWrapper = document.createElement('div');
+    osCheckboxWrapper.className = 'os-wrapper';
+    osCheckboxWrapper.innerHTML = `
+    <input type="checkbox">
+    <div class="os-name">${os}</div>`
+    return osCheckboxWrapper;
+}
+
+// Fills filter by rendered os checkboxes.
+const fillOsFlter = function(elements){
+    elements.forEach(element => { 
+        document.querySelector('.os-panel').appendChild(renderOsCheckbox(element))
+    });
+}
+
+fillOsFlter(getOs(items));
+
+
+
+
+
+
+const displayFilterArr = [
+    '2-5 inch',
+    '5-7 inch',
+    '7-12 inch',
+    '12-16 inch',
+    '+16 inch'
+];
+
+// Renders display checkbox in the filter
+const renderDisplayCheckbox = function(display){
+    const displayCheckboxWrapper = document.createElement('div');
+    displayCheckboxWrapper.className = 'display-wrapper';
+    displayCheckboxWrapper.innerHTML = `
+    <input type="checkbox">
+    <div class="memory-name">${display}</div>`
+    return displayCheckboxWrapper;
+}
+
+// Fills filter by rendered display checkboxes.
+const fillDisplayFlter = function(elements){
+    elements.forEach(element => { 
+        document.querySelector('.panel-filter-display').appendChild(renderDisplayCheckbox(element))
+    });
+}
+
+fillDisplayFlter(displayFilterArr);
